130166c597e2f24dbef25b1924d31af4230a1053
genieaitch <hah4815@naver.com>
2025-01-15 11:53:54 +0900

    수정
    수정

---
+++ main/java/com/teamzipup/zipup/ZipupApplication.java
@@ -0,0 +1,14 @@
+package com.teamzipup.zipup;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZipupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZipupApplication.class, args);
+	}
+
+}
